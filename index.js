56a328d48fc72eab83545920e136454ac200dba6
natedrake <john1o1grady@hotmail.com>
2015-11-09 22:57:54 +0000

    Initial creation of express nodejs project

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 /**
  *  index.js
- *
- *
+ *  @author John O'Grady
+ *  @note index javascript file, for routing request to correct file
  */
+
+/** Port server will listen for requests on **/
+ var PORT = 80;
+
+ var express = require('express');
+ var app = express();
+
+/** routes... **/
+ app.get('/', function(request, response) {
+     response.sendFile(__dirname + '/public/views/index.html');
+ });
+
+/** start an instance of the app/server **/
+ var server = app.listen(PORT, function () {
+     var hostname = server.address().address;
+     var port = server.address().port;
+     console.log('App listening at http://%s%s', hostname, port);
+ });
