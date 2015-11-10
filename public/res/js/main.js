7f34a610a6da66adc9db33cbf03a34a04c4e91ac
natedrake <john1o1grady@hotmail.com>
2015-11-10 17:25:40 +0000

    update ajax post with serialized data and output of response

--- public/res/js/main.js
+++ public/res/js/main.js
@@ -4,6 +4,11 @@
  *  @date 10/11/2015
  *  @note controls ajax calls
  */
+
+function caesar() {
+
+}
+
 $(function(){
     /**
      *  @note prevent default on forms on submit event to stop page refreshing on submit.
@@ -11,6 +16,14 @@ $(function(){
     $('form').each(function(index) {
         $(this).submit(function(event) {
             event.preventDefault();
+        });
+    });
+
+    $('#encrypt-form').submit(function() {
+        console.log('submitted');
+        var formData = ($(this).serializeArray());
+        $.post('/test', formData, function(data) {
+            $('#output-panel').html("<h2>Your encrypted message is:</h2><p>" + data + "</p>");
         });
     });
 });
