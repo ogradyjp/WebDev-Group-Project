a287c741bc6cbb2990d338adda0b8a59e9731727
natedrake <john1o1grady@hotmail.com>
2015-11-16 19:52:57 +0000

    insert two default requests into requests relation

--- assets/sql/previous_requests.sql
+++ assets/sql/previous_requests.sql
@@ -1,7 +1,13 @@
-create table if not exists previous_request (
+drop table if exists requests;
+
+create table if not exists requests (
   id serial primary key,
   original text not null,
   encrypted text not null,
-  timedate date not null,
-  ip_address text not null
+  requested timestamp,
+  ip text not null
 );
+
+insert into requests ("original", "encrypted", "requested", "ip") values ('blah', 'blah', '2015/11/15', '46.25.64.89');
+
+insert into requests ("original", "encrypted", "requested", "ip") values ('blah', 'blah', '15/11/2015', '46.25.64.89');
